74ce4ac2a4639015a5894ce39bc9c5b64998dcb1
unknown <vruddhi.more1995gmail.com>
2026-06-26 11:53:12 +0530

    user login with access refresh token stored in cookie and react memory

--- employeeHub-api-repo/src/auth/auth.controller.ts
+++ employeeHub-api-repo/src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res, Req, BadRequestException } from '@nestjs/common';
+import type { Response, Request } from 'express';
 import { AuthService } from './auth.service';
+import type { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,17 +10,56 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  async login(@Body() body: any) {
-    return this.authService.login(
+  async login(@Body() body: LoginDto,@Res({ passthrough: true }) res: Response,) {
+   const result =  await this.authService.login(
       body.email,
       body.password,
     );
+
+  const accessToken = result.accessToken;
+  const refreshToken = result.refreshToken;
+
+  res.cookie('refresh_token', refreshToken, {
+     httpOnly: true,
+  secure: false,      // localhost
+  sameSite: 'lax',    // <-- change this
+  path: '/',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  return {
+    accessToken,
+  };
   }
 
   @Post('refresh')
-  async refresh(@Body() body: any) {
-    return this.authService.refreshToken(
-      body.refreshToken,
-    );
+  async refresh(@Req() req: Request,
+  @Res({ passthrough: true }) res: Response) {
+    const refreshToken = req.cookies?.refresh_token ?? req.body?.refreshToken;
+     console.log("-----",req.cookies);
+
+    if (!refreshToken) {
+      throw new BadRequestException('Refresh token is missing');
+    }
+
+    const result = await this.authService.refreshToken(refreshToken);
+
+    return {
+      accessToken: result.accessToken,
+      refreshToken: result.refreshToken,
+    };
   }
 }
+
+// @Post('logout')
+// logout(
+//   @Res({ passthrough: true }) res: Response,
+// ) {
+//   res.clearCookie('refresh_token', {
+//     path: '/auth/refresh',
+//   });
+
+//   return {
+//     message: 'Logged out',
+//   };
+// }
--- employeeHub-api-repo/src/main.ts
+++ employeeHub-api-repo/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,7 +9,7 @@ app.setGlobalPrefix('api');
     origin: 'http://localhost:5173',
     credentials: true,
   });
-
+app.use(cookieParser());
   await app.listen(5000);
 }
 bootstrap();
